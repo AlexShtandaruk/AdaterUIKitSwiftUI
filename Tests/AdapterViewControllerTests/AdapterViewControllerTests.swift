5ba7771501c66b58c2d723cbb2ba6f257070e76f
AlexShtandaruk <125973696+AlexShtandaruk@users.noreply.github.com>
2023-06-22 15:12:03 +0300

    Initial Commit

---
+++ Tests/AdapterViewControllerTests/AdapterViewControllerTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import AdapterViewController
+
+final class AdapterViewControllerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AdapterViewController().text, "Hello, World!")
+    }
+}
+
